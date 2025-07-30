6dd0d7baaa2c41a88ffe84c0e8c5087a9566ea78
SHETTY1941 <shettybhavanivaraprasad@gmail.com>
2025-07-30 15:26:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World from Node.js!\n');
+  res.end('Hello, World from Shetty!\n');
 });
 
 const PORT = process.env.PORT || 3000;
